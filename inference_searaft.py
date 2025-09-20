775ef22367ecbba64fec65a79b400a64c2cca144
TanKaiYi0220 <xiaopihaikaiyi@gmail.com>
2025-09-20 23:59:44 +0800

    feat: inference_searaft.py test on entire dataset

---
+++ inference_searaft.py
@@ -0,0 +1,231 @@
+import os
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+from src.gameData_loader import load_backward_velocity
+from src.warp_module import Warping
+from src.utils import find_max_index_in_dir, save_img, EXR_to_PNG
+
+import sys
+sys.path.append('models/SEARAFT/core')
+
+import argparse
+import cv2
+import math
+import numpy as np
+import json
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+
+from models.SEARAFT.config.parser import parse_args
+
+from models.SEARAFT.core.raft import RAFT
+from models.SEARAFT.core.utils.flow_viz import flow_to_image
+from models.SEARAFT.core.utils.utils import load_ckpt
+
+from tqdm import tqdm
+
+def create_color_bar(height, width, color_map):
+    """
+    Create a color bar image using a specified color map.
+
+    :param height: The height of the color bar.
+    :param width: The width of the color bar.
+    :param color_map: The OpenCV colormap to use.
+    :return: A color bar image.
+    """
+    # Generate a linear gradient
+    gradient = np.linspace(0, 255, width, dtype=np.uint8)
+    gradient = np.repeat(gradient[np.newaxis, :], height, axis=0)
+
+    # Apply the colormap
+    color_bar = cv2.applyColorMap(gradient, color_map)
+
+    return color_bar
+
+def add_color_bar_to_image(image, color_bar, orientation='vertical'):
+    """
+    Add a color bar to an image.
+
+    :param image: The original image.
+    :param color_bar: The color bar to add.
+    :param orientation: 'vertical' or 'horizontal'.
+    :return: Combined image with the color bar.
+    """
+    if orientation == 'vertical':
+        return cv2.vconcat([image, color_bar])
+    else:
+        return cv2.hconcat([image, color_bar])
+
+def vis_heatmap(name, image, heatmap):
+    # theta = 0.01
+    # print(heatmap.max(), heatmap.min(), heatmap.mean())
+    heatmap = heatmap[:, :, 0]
+    heatmap = (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min())
+    # heatmap = heatmap > 0.01
+    heatmap = (heatmap * 255).astype(np.uint8)
+    colored_heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    overlay = image * 0.3 + colored_heatmap * 0.7
+    # Create a color bar
+    height, width = image.shape[:2]
+    color_bar = create_color_bar(50, width, cv2.COLORMAP_JET)  # Adjust the height and colormap as needed
+    # Add the color bar to the image
+    overlay = overlay.astype(np.uint8)
+    combined_image = add_color_bar_to_image(overlay, color_bar, 'vertical')
+    cv2.imwrite(name, cv2.cvtColor(combined_image, cv2.COLOR_RGB2BGR))
+
+def get_heatmap(info, args):
+    raw_b = info[:, 2:]
+    log_b = torch.zeros_like(raw_b)
+    weight = info[:, :2].softmax(dim=1)              
+    log_b[:, 0] = torch.clamp(raw_b[:, 0], min=0, max=args.var_max)
+    log_b[:, 1] = torch.clamp(raw_b[:, 1], min=args.var_min, max=0)
+    heatmap = (log_b * weight).sum(dim=1, keepdim=True)
+    return heatmap
+
+def forward_flow(args, model, image1, image2):
+    output = model(image1, image2, iters=args.iters, test_mode=True)
+    flow_final = output['flow'][-1]
+    info_final = output['info'][-1]
+    return flow_final, info_final
+
+def calc_flow(args, model, image1, image2):
+    img1 = F.interpolate(image1, scale_factor=2 ** args.scale, mode='bilinear', align_corners=False)
+    img2 = F.interpolate(image2, scale_factor=2 ** args.scale, mode='bilinear', align_corners=False)
+    H, W = img1.shape[2:]
+    flow, info = forward_flow(args, model, img1, img2)
+    flow_down = F.interpolate(flow, scale_factor=0.5 ** args.scale, mode='bilinear', align_corners=False) * (0.5 ** args.scale)
+    info_down = F.interpolate(info, scale_factor=0.5 ** args.scale, mode='area')
+    return flow_down, info_down
+
+def warping(warping_module, src_image, target_image, flow, path, name):
+    # demo warping function
+    warping_module.warp(src_image, flow)
+    warped_img, _ = warping_module.get_warping_result(mode="average")
+    hit_vis = warping_module.visualize_hit()
+    diff_img = np.abs(warped_img.cpu().numpy() - target_image.cpu().numpy())
+
+    cv2.imwrite(f"{path}warped_img_{name}.jpg", warped_img.cpu().numpy()[0].transpose(1, 2, 0).astype(np.uint8))
+    cv2.imwrite(f"{path}hit_img_{name}.jpg", hit_vis)
+    cv2.imwrite(f"{path}diff_img_{name}.jpg", diff_img[0].transpose(1, 2, 0))
+
+@torch.no_grad()
+def inference(name, args, model, warping_module, image1, image2, bmv):
+    path = f"output/{name}/"
+    os.system(f"mkdir -p {path}")
+    H, W = image1.shape[2:]
+    cv2.imwrite(f"{path}image1.jpg", image1[0].permute(1, 2, 0).cpu().numpy())
+    cv2.imwrite(f"{path}image2.jpg", image2[0].permute(1, 2, 0).cpu().numpy())
+
+    flow, info = calc_flow(args, model, image2, image1)
+    flow_vis = flow_to_image(flow[0].permute(1, 2, 0).cpu().numpy(), convert_to_bgr=False)
+    cv2.imwrite(f"{path}flow_final.jpg", flow_vis)
+    
+    heatmap = get_heatmap(info, args)
+    vis_heatmap(f"{path}heatmap_final.jpg", image1[0].permute(1, 2, 0).cpu().numpy(), heatmap[0].permute(1, 2, 0).cpu().numpy())
+    
+
+    # forward warp for flow direction check
+    warping(warping_module, image2, image1, flow, path, name="opticalFlow_backward")
+
+    # backward warping for flow direction check
+    warping(warping_module, image2, image1, bmv, path, name="gameMotion_backward")
+
+    bmv_vis = flow_to_image(bmv[0].permute(1, 2, 0).cpu().numpy(), convert_to_bgr=False)
+    cv2.imwrite(f"{path}flow_gameData.jpg", bmv_vis)
+
+    epe = torch.sum((flow - bmv)**2, dim=1).sqrt()
+
+    return epe.mean().cpu().item()
+
+def FRPG_loader(name, model, warping_module, dataset_dir_path, args):
+    max_index = find_max_index_in_dir(dataset_dir_path)
+    print(max_index)
+
+    epe_scores = []
+
+    with tqdm(range(max_index - 1)) as pbar:
+        for i in pbar:
+            image_1_path = f"{dataset_dir_path}colorNoScreenUI_{i}.exr"
+            image_2_path = f"{dataset_dir_path}colorNoScreenUI_{i + 1}.exr"
+            image_1 = EXR_to_PNG(image_1_path)
+            image_2 = EXR_to_PNG(image_2_path)
+
+            save_img(f"{dataset_dir_path}colorNoScreenUI_{i}.png", image_1)
+            save_img(f"{dataset_dir_path}colorNoScreenUI_{i + 1}.png", image_2)
+
+            image_1 = cv2.imread(f"{dataset_dir_path}colorNoScreenUI_{i}.png")
+            image_2 = cv2.imread(f"{dataset_dir_path}colorNoScreenUI_{i + 1}.png")
+
+            image_1 = torch.from_numpy(image_1).permute(2, 0, 1).unsqueeze(0).float().cuda()
+            image_2 = torch.from_numpy(image_2).permute(2, 0, 1).unsqueeze(0).float().cuda()
+
+            bmv_0_path = f"{dataset_dir_path}backwardVel_Depth_{i + 1}.exr"
+            bmv, depth = load_backward_velocity(bmv_0_path)
+
+            file_name = f"{name}/results/frame_{i:04d}_to_{i+1:04d}/"
+
+            epe = inference(file_name, args, model, warping_module, image_1, image_2, bmv)
+            pbar.set_postfix({"EPE": f"{epe:.4f}"})
+
+            epe_scores.append(epe)
+
+    # Prepare JSON result
+    max_epe = max(epe_scores)
+    min_epe = min(epe_scores)
+    max_idx = epe_scores.index(max_epe)
+    min_idx = epe_scores.index(min_epe)
+
+    results = {
+        "exp_config": {
+            "dataset_dir": dataset_dir_path,
+            "model_args": {
+                "cfg": args.cfg,
+                "model": args.model,
+            },
+        },
+        "epe_results": {
+            "mean": sum(epe_scores) / len(epe_scores),
+            "max": {
+                "score": max_epe,
+                "index": max_idx
+            },
+            "min": {
+                "score": min_epe,
+                "index": min_idx
+            },
+            "all": [
+                {
+                    "score": score,
+                    "index": idx,
+                } for idx, score in enumerate(epe_scores)
+            ]
+
+        }
+    }
+
+    # Save to JSON file
+    with open(f"output/{name}/exp_results.json", "w") as f:
+        json.dump(results, f, indent=4)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', help='experiment configure file name', required=True, type=str)
+    parser.add_argument('--model', help='checkpoint path', required=True, type=str)
+    # parser.add_argument('')
+    args = parse_args(parser)
+    model = RAFT(args)
+    load_ckpt(model, args.model)
+    model = model.cuda()
+    model.eval()
+
+    warping_module = Warping(warp_mode="nearest")
+
+    # template for loading FRPG images
+    dataset_dir_path = "/home/kevin/Desktop/VFI/offline_dataset/datasets/AnimeFantasyRPG/0_Easy/0_Easy_0/fps_30/"
+    FRPG_loader("SEARAFT/AnimeFantasyRPG/0_Easy/0_Easy_0/fps_30/", model, warping_module, dataset_dir_path, args)
+
+
+if __name__ == '__main__':
+    main()
--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,34 @@ import os
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import re
+from .EXR_loader import loadEXR
 
 def save_img(path: str, img: np.ndarray):
     dir_name = os.path.dirname(path)
     if not os.path.exists(dir_name):
         os.makedirs(dir_name, exist_ok=True)
     cv2.imwrite(path, img)
+
+def find_max_index_in_dir(path: str) -> int:
+    """
+    掃描指定資料夾，回傳檔名中最大的 index 整數。
+    """
+    max_index = -1
+    pattern = re.compile(r'_(\d+)\.')  # 抓取最後一個 '_' 與 '.' 之間的數字
+
+    for filename in os.listdir(path):
+        match = pattern.search(filename)
+        if match:
+            index = int(match.group(1))
+            if index > max_index:
+                max_index = index
+
+    return max_index
+
+def EXR_to_PNG(filepath: str):
+    exrImg = loadEXR(filepath)
+    exrImg = exrImg * 255.0
+    exrImg = exrImg.astype(np.uint8)  # Fixed: assign the conversion
+
+    return exrImg
